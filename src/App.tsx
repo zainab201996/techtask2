0015ba9e8899904154a223d83cf68a3dabb7367c
hasanaliqureshi <hasanali.zeds@gmail.com>
2024-03-23 01:22:43 +0500

    tasks added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-
-import './App.css';
-
+import "./App.css";
 
 function App() {
+  //TASK 1 COUNTER
 
- 
+  //TEST 2 FETCHING USER
+  // https://randomuser.me/api
+
+  //TEST 3 FETCHING USER WITH PAGINATION
+  // https://randomuser.me/api?page=1&result=1
+
   return (
     <div className="App">
-      <p>Hello World!</p>
+      <p>COUNTER: </p>
+      <button>Increment</button>
+      <button>Decrement</button>
+      <hr />
+      
     </div>
   );
 }
