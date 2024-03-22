3c4eb72e28e20a11233a7ae3123426a9651f2772
hasanaliqureshi <hasanali.zeds@gmail.com>
2024-03-23 01:15:43 +0500

    changes

---
+++ src/components/User.tsx
@@ -0,0 +1,16 @@
+
+const User = () => {
+
+
+  return (
+    <div>
+      <p>First Name: </p>
+      <p>Last Name: </p>
+      <p>City: </p>
+      <p>State: </p>
+      <p>Country: </p>
+    </div>
+  );
+};
+
+export default User;
